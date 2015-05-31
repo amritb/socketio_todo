53b345cfbf50febc84b4813697f75fd7c5fa436b
amritb <amritbera@gmail.com>
2015-05-31 12:58:52 +0530

    socket io calls actions now instead of directly dispatching

--- frontend/app/socketio/socketio.js
+++ frontend/app/socketio/socketio.js
@@ -1,12 +1,8 @@
-var AppDispatcher = require('../dispatcher/AppDispatcher');
-var appContants = require('../constants/todoConstants');
+var todoAction = require('../actions/todoActions');
 var io = require('socket.io-client');
 
 var socket = io('http://localhost:8080');
 
 socket.on('newItem', function (data) {
-  AppDispatcher.dispatch({
-    actionType: appContants.ADD_ITEM,
-    data: data.newItem
-  });
+  todoAction.addItem(data);
 });
